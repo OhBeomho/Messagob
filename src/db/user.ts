449d32b87a7f7b38b8d21a60408e0b47a3406ece
OhBeomho <tyler0322@naver.com>
2023-01-14 15:23:14 +0900

    reject error

--- src/db/user.ts
+++ src/db/user.ts
@@ -20,9 +20,9 @@ export class User {
 
 	static async createUser(username: string, password: string) {
 		if (!await UserManager.checkUsername(username)) {
-			return Promise.reject("이미 존재하는 사용자입니다.");
+			throw new Error("이미 존재하는 사용자입니다.");
 		} else if (username.length < 3) {
-			return Promise.reject("사용자명은 3자 이상이여야 합니다.");
+			throw new Error("사용자명은 3자 이상이여야 합니다.");
 		}
 
 		await db.query(`INSERT INTO "user"(username, password) VALUES($1, $2)`, [username, password]);
@@ -38,15 +38,15 @@ export class User {
 
 	async requestFriend(targetUsername: string) {
 		if (!(await db.query(`SELECT EXISTS(SELECT * FROM "user" WHERE username = $1)`, [targetUsername])).rows[0].exists) {
-			return Promise.reject("존재하지 않는 사용자입니다.");
+			throw new Error("존재하지 않는 사용자입니다.");
 		}
 
 		const { friendrequests: friendRequests, friends } = (await db.query(`SELECT friendrequests, friends FROM "user" WHERE username = $1`, [targetUsername])).rows[0];
 
 		if (friendRequests.includes(this.username)) {
-			return Promise.reject("이미 요청을 보냈습니다.");
+			throw new Error("이미 요청을 보냈습니다.");
 		} else if (friends.includes(this.username)) {
-			return Promise.reject("이미 친구입니다.");
+			throw new Error("이미 친구입니다.");
 		}
 
 		await db.query(`UPDATE "user" SET friendrequests = ARRAY_APPEND(friendrequests, $1) WHERE username = $2`, [this.username, targetUsername]);
@@ -54,7 +54,7 @@ export class User {
 
 	async accept(requestUsername: string) {
 		if (!(await db.query(`SELECT EXISTS(SELECT * FROM "user" WHERE username = $1 AND $2 = ANY(friendrequests))`, [this.username, requestUsername]))) {
-			return Promise.reject(requestUsername + "님의 요청이 없습니다.");
+			throw new Error(requestUsername + "님의 요청이 없습니다.");
 		} else {
 			await db.query(`UPDATE "user" SET friendrequests = ARRAY_REMOVE(friendrequests, $1) WHERE username = $2`, [requestUsername, this.username]);
 			await db.query(`UPDATE "user" SET friends = ARRAY_APPEND(friends, $1) WHERE username = $2`, [requestUsername, this.username]);
@@ -64,7 +64,7 @@ export class User {
 
 	async decline(requestUsername: string) {
 		if (!(await db.query(`SELECT EXISTS(SELECT * FROM "user" WHERE username $1 SND $2 = ANY(friendrequests))`, [this.username, requestUsername]))) {
-			return Promise.reject(requestUsername + "님의 요청이 없습니다.");
+			throw new Error(requestUsername + "님의 요청이 없습니다.");
 		} else {
 			await db.query(`UPDATE "user" SET friendrequests = ARRAY_REMOVE(friendrequests, $1) WHERE username = $2`, [requestUsername, this.username]);
 		}
@@ -73,7 +73,7 @@ export class User {
 	async inviteRoom(roomID: number) {
 		const exists = (await ChatRoomManager.getChatRoom(roomID)).id !== -1;
 		if (!exists) {
-			return Promise.reject("존재하지 않는 방입니다.");
+			throw new Error("존재하지 않는 방입니다.");
 		}
 
 		await db.query(`UPDATE "user" SET roomrequests = ARRAY_APPEND(roomrequests, $1) WHERE username = $2`, [roomID, this.username]);
@@ -84,7 +84,7 @@ export class User {
 		const { roomrequests } = rows[0];
 
 		if (!roomrequests.includes(roomID)) {
-			return Promise.reject("초대받지 않거나 존재하지 않는 방입니다.");
+			throw new Error("초대받지 않거나 존재하지 않는 방입니다.");
 		}
 
 		await db.query(`UPDATE "user" SET roomrequests = ARRAY_REMOVE(roomrequests, $1) WHERE username = $2`, [roomID, this.username]);
@@ -94,9 +94,9 @@ export class User {
 	async leaveRoom(roomID: number) {
 		const room = await ChatRoomManager.getChatRoom(roomID);
 		if (room.id === -1) {
-			return Promise.reject("존재하지 않는 방입니다.");
+			throw new Error("존재하지 않는 방입니다.");
 		} else if (!room.users.includes(this.username)) {
-			return Promise.reject("방 인원이 아닙니다.");
+			throw new Error("방 인원이 아닙니다.");
 		}
 
 		await db.query("UPDATE chatroom SET users = ARRAY_REMOVE(users, $1) WHERE id = $2", [this.username, roomID]);
@@ -111,9 +111,9 @@ export class UserManager {
 	static async checkPassword(username: string, password: string) {
 		const { rows } = (await db.query(`SELECT password FROM "user" WHERE username = $1`, [username]));
 		if (!rows[0]) {
-			return Promise.reject("존재하지 않는 사용자입니다.");
+			throw new Error("존재하지 않는 사용자입니다.");
 		} else if (rows[0].password !== password) {
-			return Promise.reject("비밀번호가 일치하지 않습니다.");
+			throw new Error("비밀번호가 일치하지 않습니다.");
 		}
 	}
 }
