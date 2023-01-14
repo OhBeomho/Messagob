5c737c3493ad9a50a31e7cf6a59dabac0caecd2b
OhBeomho <tyler0322@naver.com>
2023-01-14 09:41:12 +0900

    static files

---
+++ public/js/chat.js
@@ -0,0 +1,9 @@
+/*
+	TODO:
+
+	Chat feature.
+
+	When page loads, read messages from '/chat/list/:roomid' and do JSON.parse(), then show messages.
+	Listen to 'chat' event on socket.io.
+	When user sends chat, send request to '/chat/save'.
+*/
