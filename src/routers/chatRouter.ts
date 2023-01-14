58486e165fea6631614bac517b4d77124df1336d
OhBeomho <tyler0322@naver.com>
2023-01-14 09:43:51 +0900

    Saving message Message list

--- src/routers/chatRouter.ts
+++ src/routers/chatRouter.ts
@@ -1,9 +1,30 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { ChatRoomManager } from "../db/chatroom";
+import { MessageManager } from "../db/message";
+import wrap from "./asyncWrapper";
 
 const router = Router();
 
-router.post("/save", (req, res) => {
-	// TODO: Save message
+router.post("/save", (req, _) => {
+	if (!req.session.user) {
+		return;
+	}
+
+	const { content } = req.body;
+	const { username } = req.session.user;
+
+	MessageManager.save(username, content);
 });
+
+router.get("/list/:roomid", wrap(async (req: Request, res: Response, next: NextFunction) => {
+	const { roomid } = req.params;
+	const room = await ChatRoomManager.getChatRoom(parseInt(roomid));
+
+	if (room.id === -1) {
+		res.sendStatus(404);
+	} else {
+		res.send(room.messages);
+	}
+}));
 
 export default router;
