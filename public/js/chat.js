c151a16d87c369f917255c4684eb310073acc1b0
OhBeomho <tyler0322@naver.com>
2023-01-18 09:49:05 +0900

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,26 @@
-/*
-	TODO:
+socket.on("message", (data) => {
+	if (data.roomID !== currentRoomID) {
+		// TODO: 방 메시지 알림
+	}
 
-	Chat feature.
+	// TODO: 메시지 목록에 메시지 추가
+});
 
-	When page loads, read messages from '/chat/list/:roomid' and do JSON.parse(), then show messages.
-	Listen to 'chat' event on socket.io.
-	When user sends chat, send request to '/chat/save'.
-*/
+messageInput.addEventListener("keydown", (e) => {
+	if (!e.repeat && !e.shiftKey && e.key === "Enter") {
+		e.preventDefault();
+		sendMessage(messageInput.value);
+	}
+});
+sendButton.addEventListener("click", () => sendMessage(messageInput.value));
+
+function sendMessage(message) {
+	if (!message) {
+		return;
+	}
+
+	socket.emit("message", {
+		message,
+		roomID: currentRoomID
+	});
+}
