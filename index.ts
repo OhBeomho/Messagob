494fbd2a4a8cbe197a6c58b9eb69e829749eff9a
OhBeomho <tyler0322@naver.com>
2023-01-18 09:48:14 +0900

    Saving message Added new router Changed user session data type

--- index.ts
+++ index.ts
@@ -5,8 +5,11 @@ import fileStore from "session-file-store";
 import http from "http";
 import { Server } from "socket.io";
 import userRouter from "./src/routers/userRouter";
+import roomRouter from "./src/routers/roomRouter";
 import chatRouter from "./src/routers/chatRouter";
 import { User } from "./src/db/user";
+import { MessageManager } from "./src/db/message";
+import { ChatRoomManager } from "./src/db/chatroom";
 
 const app = express();
 const server = http.createServer(app);
@@ -16,19 +19,20 @@ const session = express_session({
 	secret: String(process.env.SESSION_SECRET),
 	resave: false,
 	saveUninitialized: true,
-	store: new FileStore()
+	store: new FileStore(),
+	name: "connect.sid"
 });
 
 declare module "express-session" {
 	export interface SessionData {
-		user?: User;
+		user?: string;
 	}
 }
 
 declare module "http" {
 	export interface IncomingMessage {
 		session: Session & {
-			user?: User;
+			user?: string;
 		}
 	}
 }
@@ -39,26 +43,45 @@ app.use(express.urlencoded({ extended: true }));
 app.use(session);
 
 app.use("/user", userRouter);
+app.use("/room", roomRouter);
 app.use("/chat", chatRouter);
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.get("/", (req, res) => res.render("index", { user: req.session.user }));
+app.get("/", (req, res) => {
+	if (!req.session.user) {
+		res.render("index", { user: undefined });
+		return;
+	}
+
+	User.getUser(req.session.user)
+		.then((user) => res.render("index", { user }));
+});
 
 io.use((socket, next) => session(socket.request as Request, {} as Response, next as NextFunction));
 
 io.on("connection", (socket) => {
 	const user = socket.request.session.user;
-	user && socket.join(user.username);
+	let currentRoomID = -1;
 
 	socket.on("message", (data) => {
-		if (!user) return;
+		if (!user || currentRoomID === -1) return;
 
-		io.to(data.username).to(user.username).emit("message", {
-			username: user.username,
+		io.to(data.roomID).emit("message", {
+			username: user,
 			message: data.message,
+			roomID: currentRoomID
 		});
+
+		MessageManager.save(user, data.message)
+			.then((message) => ChatRoomManager.addMessage(parseInt(data.roomID), message));
+	});
+	socket.on("room", (roomID) => {
+		socket.leave(String(currentRoomID));
+		socket.join(String(roomID));
+
+		currentRoomID = roomID;
 	});
 });
 
