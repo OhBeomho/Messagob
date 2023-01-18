66e93ca8106167da9fb17e3041d3cd308e6c9f1c
OhBeomho <tyler0322@naver.com>
2023-01-18 09:50:12 +0900

    Update message.ts

--- src/db/message.ts
+++ src/db/message.ts
@@ -1,3 +1,4 @@
+import { ChatRoomManager } from "./chatroom";
 import db from "./database";
 
 export interface Message {
@@ -12,28 +13,22 @@ export class MessageManager {
 		const { rows } = await db.query("SELECT content, t, username FROM message WHERE id = $1", [id]);
 		if (!rows[0]) {
 			return {} as Message;
-		} else {
-			const { content, t: time, username } = rows[0];
-			return {
-				id,
-				content,
-				time,
-				username
-			} as Message;
 		}
+
+		const { content, t: time, username } = rows[0];
+		return {
+			id,
+			content,
+			time,
+			username
+		} as Message;
 	}
 
 	static async save(username: string, content: string) {
-		if (!username || !content) {
-			return;
-		}
-
-		await db.query("INSERT INTO message(username, content, t) VALUES($1, $2, $3)", [username, content, new Date()]);
+		return (await db.query("INSERT INTO message(username, content, t) VALUES($1, $2, $3) RETURNING *", [username, content, new Date()])).rows[0] as Message;
 	}
 
-	static async *list(messages: number[]) {
-		for (let message of messages) {
-			yield await MessageManager.getMessage(message);
-		}
+	static async list(messageIDArray: number[]) {
+		return (await db.query("SELECT * FROM message WHERE id = ANY($1)", [messageIDArray])).rows as Message[];
 	}
 }
